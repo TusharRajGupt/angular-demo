13a35c0eecc017ff4a04c77555150c207de15a6a
Tushar Raj <tushar@levelops.io>
2020-03-02 21:50:35 +0530

    fleshed out form

--- session-0/tutorial/src/app/login/login.component.ts
+++ session-0/tutorial/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
@@ -8,20 +8,37 @@ import { FormBuilder } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
     loginForm = this.fb.group({
-        username: [''],
-        password: [''],
+        username: ['', [Validators.required, Validators.minLength(6)]],
+        password: ['', [Validators.required]],
     });
 
     passwordHidden = true;
+    isFormValid: boolean;
 
     constructor(private fb: FormBuilder) { }
 
     ngOnInit(): void {
+        this.isFormValid = this.loginForm.status === 'VALID';
     }
 
     onSubmit() {
         console.log('submitted');
-        console.log(this.loginForm.value);
+        console.log(this.loginForm.status);
+        console.log(this.isFormValid);
     }
+    
+    getUsernameError() {
+        console.log(this.isFormValid);
+        console.log(this.loginForm.status);
+        
+        if (this.username.hasError('required')) {
+            return 'You must enter a value';
+        } else if (this.username.hasError('minlength')) {
+            return 'At least 6 characters required';
+        }
+    }
+
+    get username() { return this.loginForm.get('username'); }
+    get password() { return this.loginForm.get('password'); }
 
 }
