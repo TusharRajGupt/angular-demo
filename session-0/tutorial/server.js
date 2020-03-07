e640b07686882a7cf7279aa80ef64c908bad04d7
Tushar Raj <tushar@levelops.io>
2020-03-07 11:07:00 +0530

    improved server

--- session-0/tutorial/server.js
+++ session-0/tutorial/server.js
@@ -1,12 +1,52 @@
-const exp = require('./backend/express');
+const app = require('./backend/express');
+const debug = require('debug')('node-angular');
 const http = require('http');
 
-const port = process.env.PORT || 2300;
+const normalizePort = val => {
+  var port = parseInt(val, 10);
 
-exp.set('port', port)
-const server = http.createServer(exp);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-console.log('ran');
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
+  return false;
+};
 
+const onError = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
+  debug('Listening on ' + bind);
+};
+
+const port = normalizePort(process.env.PORT || '2300');
+app.set('port', port);
+
+const server = http.createServer(app);
+server.on('error', onError);
+server.on('listening', onListening);
 server.listen(port);
